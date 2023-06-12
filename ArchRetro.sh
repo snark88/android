cc498eabd7893a03d098d1f85bfd4f66bccd71d0
snark88 <123511677+snark88@users.noreply.github.com>
2023-06-13 00:19:28 +0200

    Update ArchRetro.sh

--- ArchRetro.sh
+++ ArchRetro.sh
@@ -1,21 +1,19 @@
 #!/bin/bash
- 
-adb shell echo "kobles til Android Device via ADB" || exit 1
- 
-echo "Downloading Retro Arch..."
-wget -c "https://buildbot.libretro.com/stable/1.14.0/android/RetroArch.apk"
- 
-echo "installerer Retro Arch..."
-adb install -g RetroArch.apk
-adb shell am start -n com.retroarch/.browser.mainmenu.MainMenuActivity
- 
-echo "laster ned ROMS..."
-adb shell mkdir -p /sdcard/roms/nes
-mkdir /tmp/nes
-cd /tmp/nes
+
+echo "Downloading RetroArch..."
+pkg install -y wget
+wget -c "https://buildbot.libretro.com/stable/1.14.0/android/RetroArch.apk" -P ~/storage/downloads/
+
+echo "Installing RetroArch..."
+pkg install -y termux-api
+termux-open ~/storage/downloads/RetroArch.apk
+
+echo "Downloading ROMs..."
+pkg install -y unzip
+mkdir -p ~/storage/roms/nes
+cd ~/storage/roms/nes
 wget -c "https://archive.org/compress/nes-rom-collection" -O nes.zip
-unzip nes.zip
-adb push *.nes /sdcard/roms/nes/
- 
-cd
-rm -fr /tmp/nes
+unzip nes.zip "*.nes"
+rm nes.zip
+
+echo "Script execution completed."
